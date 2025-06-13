56f2ee8563072cac92fa807c5e38ab24d9fbaafc
Dpati10 <diegopat1402@gmail.com>
2025-06-12 18:19:20 -0600

    Semana 4
    Commit se sem 4

---
+++ Tienda/src/main/java/com/tienda/repositories/ProductoRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.repositories;
+import com.tienda.entities.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author diego
+ */
+public interface ProductoRepository extends CrudRepository<Producto, Long> {
+    
+}
